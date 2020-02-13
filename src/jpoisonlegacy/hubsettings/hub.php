992fa8020b46ed23748d2e60e5386816fa91a1e7
JPoisonLegacy <61003023+JPoisonLegacy@users.noreply.github.com>
2020-02-13 23:01:00 +0800

    Update hub.php

--- src/jpoisonlegacy/hubsettings/hub.php
+++ src/jpoisonlegacy/hubsettings/hub.php
@@ -16,14 +16,43 @@ class hub extends PluginBase implements Listener {
     $this->getLogger()->info("Hub-Plugin-for-DreamWorld-ENABLED")
   }
   
-  public function onInteract(PlayerInteractEvent $event)
-	{
+  public function onInteract(PlayerInteractEvent $event){
     $player = $event->getPlayer();
     $item = $event->getItem();
     $tile = $player->getLevel()->getTile($item);
     
-    if($tile instanceof Compass)
-    {
+    if($tile instanceof Compass){
+        $api = Server::getInstance()->getPluginManager()->getPlugin("FormAPI");
+        if ($api === null || $api->isDisabled()) {
+            return;
+        }
+        $form = $api->createSimpleForm(function (Player $sender, ?int $result = null) {
+            if ($result === null) {
+                return true;
+            }
+            switch ($result) {
+                case 0:
+                    Server::getInstance()->dispatchCommand($sender, "warp 1v1");
+                    break;
+                case 1:
+                    Server::getInstance()->dispatchCommand($sender, "warp skywar");
+                    break;
+                case 2:
+                    Server::getInstance()->dispatchCommand($sender, "warp bedwar");
+                    break;
+                case 3:
+                    Server::getInstance()->dispatchCommand($sender, "warp survival");
+                    break;
+            }
+            return false;
+        });
+        $form->setTitle("§l§aGames§bList§r");
+        $form->setContent("§k!!§r§fGames§0List§r");
+        $form->addButton("1v1");
+        $form->addButton("Skywar");
+        $form->addButton("Bedwar");
+        $form->addButton("Survival");
+        $form->sendToPlayer($player);
     }
   }
 }
